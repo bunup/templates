beda438490299a2dbc8281923181af4ffbf7294c
arshad <arshadpyaseen@gmail.com>
2025-05-27 15:37:47 +0530

    chore: format

--- ts-lib-monorepo/packages/package-1/src/index.ts
+++ ts-lib-monorepo/packages/package-1/src/index.ts
@@ -1,3 +1,3 @@
 export function greet(name: string): string {
-    return `Hello, ${name}! Welcome to the package-1 package.`;
+	return `Hello, ${name}! Welcome to the package-1 package.`
 }
--- ts-lib-monorepo/packages/package-1/test/index.test.ts
+++ ts-lib-monorepo/packages/package-1/test/index.test.ts
@@ -1,8 +1,6 @@
-import { expect, test } from "bun:test";
-import { greet } from "../src";
+import { expect, test } from 'bun:test'
+import { greet } from '../src'
 
-test("package-1 package should greet correctly", () => {
-    expect(greet("World")).toBe(
-        "Hello, World! Welcome to the package-1 package.",
-    );
-});
+test('package-1 package should greet correctly', () => {
+	expect(greet('World')).toBe('Hello, World! Welcome to the package-1 package.')
+})
