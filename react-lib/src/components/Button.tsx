586415a14f1f13d6dd6209a60f9719f46404a495
arshad <arshadpyaseen@gmail.com>
2025-08-18 23:39:37 +0530

    chore: update

--- react-lib/src/components/Button.tsx
+++ react-lib/src/components/Button.tsx
@@ -2,6 +2,6 @@
 
 import type { PropsWithChildren } from 'react'
 
-export const Button = ({ children }: PropsWithChildren): React.ReactNode => {
+export function Button({ children }: PropsWithChildren): React.ReactNode {
 	return <button type="button">{children}</button>
 }
