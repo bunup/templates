2b4b17c18b281fa098880d863c001fd48e191b4d
arshad <arshadpyaseen@gmail.com>
2025-08-23 12:54:34 +0530

    chore: update

--- update-deps.js
+++ update-deps.js
@@ -2,19 +2,22 @@ import {$} from 'bun'
 
 const packages = [
   "react-lib",
+  "react-lib/test/ui",
   "ts-lib",
   "ts-lib-monorepo",
 ]
 
 for (const p of packages) {
   await $`cd ${p} && bun update --latest`
-  const packageJson = await Bun.file(`${p}/package.json`).json()
-  const biomeVersion = packageJson.devDependencies['@biomejs/biome'].slice(1)
-  const biomeSchema = await Bun.file(`${p}/biome.json`).json()
-  const newBiomeSchema = {
-    ...biomeSchema,
-    $schema: `https://biomejs.dev/schemas/${biomeVersion}/schema.json`,
-  }
-  await Bun.write(`${p}/biome.json`, JSON.stringify(newBiomeSchema, null, 2))
-  await $`cd ${p} && bun run lint:fix`
+  try {
+    const packageJson = await Bun.file(`${p}/package.json`).json()
+    const biomeVersion = packageJson.devDependencies['@biomejs/biome'].slice(1)
+    const biomeSchema = await Bun.file(`${p}/biome.json`).json()
+    const newBiomeSchema = {
+      ...biomeSchema,
+      $schema: `https://biomejs.dev/schemas/${biomeVersion}/schema.json`,
+    }
+    await Bun.write(`${p}/biome.json`, JSON.stringify(newBiomeSchema, null, 2))
+    await $`cd ${p} && bun run lint:fix`
+  } catch {}
 }
