37fa1f8fc7e8d649be6b4045f699e3efaa218a16
arshad <arshadpyaseen@gmail.com>
2025-08-30 03:14:51 +0530

    chore: add basic templates

---
+++ ts-lib-monorepo-basic/bunup.config.ts
@@ -0,0 +1,12 @@
+import { defineWorkspace } from 'bunup'
+
+export default defineWorkspace([
+	{
+		name: 'package-1',
+		root: 'packages/package-1',
+		config: {
+			entry: ['src/index.ts'],
+			format: ['esm', 'cjs'],
+		},
+	},
+])
