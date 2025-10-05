c26f95f69bf855f624c391427734914afc9bb1bf
arshad <arshadpyaseen@gmail.com>
2025-10-05 14:02:19 +0530

    update

--- update-deps.ts
+++ update-deps.ts
@@ -1,17 +1,17 @@
 import {$} from 'bun'
 
 const packages = [
-  "react-lib",
-  "react-lib/test/ui",
-  "ts-lib",
-  "ts-lib-monorepo",
-  "react-lib-basic",
-  "ts-lib-basic",
-  "ts-lib-monorepo-basic",
+  "full-react",
+  "full-ts",
+  "full-ts-monorepo",
+  "minimal-react",
+  "minimal-ts",
+  "minimal-ts-monorepo",
 ]
 
 for (const p of packages) {
-  await $`cd ${p} && bun update --latest`
+   await $`cd ${p} && rm -rf node_modules && rm -rf bun.lock && rm -rf dist`
+  await $`cd ${p} && bun install && bun update --latest --force`
   try {
     const packageJson = await Bun.file(`${p}/package.json`).json()
     const biomeVersion = packageJson.devDependencies['@biomejs/biome'].slice(1)
@@ -21,6 +21,6 @@ for (const p of packages) {
       $schema: `https://biomejs.dev/schemas/${biomeVersion}/schema.json`,
     }
     await Bun.write(`${p}/biome.json`, JSON.stringify(newBiomeSchema, null, 2))
-    await $`cd ${p} && bun run build && bun run lint:fix && rm -rf .husky`
+    await $`cd ${p} && bun run build && rm -rf .husky && bun run lint:fix`
   } catch {}
 }
