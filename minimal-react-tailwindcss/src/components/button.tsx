35234c2a3e7862c50e689750069c1e157f094983
arshad <arshadpyaseen@gmail.com>
2025-10-06 11:32:43 +0530

    chore: update

--- minimal-react-tailwindcss/src/components/button.tsx
+++ minimal-react-tailwindcss/src/components/button.tsx
@@ -4,7 +4,7 @@ export function Button(props: React.ComponentProps<'button'>): React.ReactNode {
 	return (
 		<button
 			type="button"
-			className="[project_name_letter_only]:bg-blue-500 [project_name_letter_only]:hover:bg-blue-600 [project_name_letter_only]:text-white [project_name_letter_only]:px-4 [project_name]:py-2 [project_name]:rounded-md"
+			className="[package_name_letters_only]:bg-blue-500 [package_name_letters_only]:hover:bg-blue-600 [package_name_letters_only]:text-white [package_name_letters_only]:px-4 [project_name]:py-2 [project_name]:rounded-md"
 			{...props}
 		/>
 	)
